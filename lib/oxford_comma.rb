b353625489965116c22c2770342e6eeeca334c86
MZidar <MZidar@gmail.com>
2021-09-22 13:31:00 -0400

    commit tree bd12fa25ca12b08755e576e609ac3e956efcebdc

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def oxford_comma(array)
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        
+
+    
+     
+    final = array.pop
+    
+    penult = array.join(", ")
+    penult << ", and #{final}"
+    return penult
+    end
 
 end
+
